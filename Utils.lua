2043d40c36b47274ef99a37f4a08ed52bff9a88c
Sebastian <seb@cixel.com>
2023-01-15 15:58:14 +0800

    Only open dialog if not already open

--- Utils.lua
+++ Utils.lua
@@ -104,6 +104,10 @@ local configDialogStatusText = format("|cff808080Version: %s • Author: %s|r", 
   Open the config dialog.
 ]]
 function Utils:OpenConfigDialog()
+  if (AceConfigDialog.OpenFrames[addonName]) then
+    return
+  end
+
   AceConfigDialog:Open(addonName)
   AceConfigDialog.OpenFrames[addonName]:SetStatusText(configDialogStatusText)
 end
