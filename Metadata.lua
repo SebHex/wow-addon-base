3dd7ae8b3bf7b34e40c55be1002a15a4406272a1
Sebastian <seb@cixel.com>
2024-03-19 18:59:37 +0800

    Replace deprecated functions

--- Metadata.lua
+++ Metadata.lua
@@ -3,9 +3,9 @@ local addonName, addonTable = ...
 local Metadata = {}
 addonTable.Metadata = Metadata
 
-Metadata.Version = GetAddOnMetadata(addonName, "version")
-Metadata.Title = GetAddOnMetadata(addonName, "title")
-Metadata.Notes = GetAddOnMetadata(addonName, "notes")
-Metadata.Author = GetAddOnMetadata(addonName, "author")
+Metadata.Version = C_AddOns.GetAddOnMetadata(addonName, "version")
+Metadata.Title = C_AddOns.GetAddOnMetadata(addonName, "title")
+Metadata.Notes = C_AddOns.GetAddOnMetadata(addonName, "notes")
+Metadata.Author = C_AddOns.GetAddOnMetadata(addonName, "author")
 Metadata.DBName = addonName:gsub("_", "") .. "DB"
 Metadata.Initials = Metadata.Title:gsub("([^%s])[^%s]*", "%1"):gsub("%s+", "")
